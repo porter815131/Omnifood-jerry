dc5b1be0b76aef833bf1c962982c32b3e6457b57
Jerry Chein <breakingkyo@gmail.com>
2021-10-26 19:08:11 +0800

    added sticky navigation

--- js/script.js
+++ js/script.js
@@ -42,3 +42,31 @@ allLinks.forEach(function (link) {
       headerEl.classList.toggle("nav-open");
   });
 });
+
+//////////////////////////////////////////////////////////////
+// Sticky navigation
+//////////////////////////////////////////////////////////////
+
+const sectionHeroEl = document.querySelector(".section-hero");
+const obs = new IntersectionObserver(
+  function (entries) {
+    const ent = entries[0];
+    console.log(ent);
+
+    if (!ent.isIntersecting) {
+      document.body.classList.add("sticky");
+    }
+
+    if (ent.isIntersecting) {
+      document.body.classList.remove("sticky");
+    }
+  },
+  {
+    // n the viewport
+    root: null,
+    threshold: 0,
+    rootMargin: "-80px",
+  }
+);
+
+obs.observe(sectionHeroEl);
